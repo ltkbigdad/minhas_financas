fc31a15d526bf0d53f31b022c07c20e30cd25c83
Douglas Braz Batista <doug.batista@live.com>
2023-01-12 19:39:56 -0300

    Feat: projeto iniciado e header construida

---
+++ src/pages/Home/index.js
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from 'react-native';
+import Header from '../../components/Header';
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+        <Header name="Douglas Braz"/>      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    
+  },
+});
