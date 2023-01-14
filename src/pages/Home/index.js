c8942bbacc7c908da79dd5a6542eae91fac051fd
Douglas Braz Batista <doug.batista@live.com>
2023-01-13 23:44:53 -0300

    feat: construção do componente card de balanços

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native';
+import Balance from '../../components/Balance';
 import Header from '../../components/Header';
 
 export default function Home() {
   return (
     <View style={styles.container}>
-        <Header name="Douglas Braz"/>      
+      <Header name="Douglas Braz"/>   
+      <Balance saldo="9.500,00" gastos="395,00"/>
+
     </View>
   );
 }
@@ -12,7 +15,7 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fafafa',
     
   },
 });
